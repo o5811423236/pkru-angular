b0a1327d9c538b557f888f21bb34495d76fa1377
your Name <you@example.com>
2018-07-23 13:27:21 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Myapp Angular ';
+  version:Number =1.0;
+  disabled:Boolean = true;
+  changeTitle(event){
+    console.log(event.target.value);
+    this.title = event.target.value;
+    //this.title = event.key;
+  }
 }
